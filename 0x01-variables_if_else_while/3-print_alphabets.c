7b0934dd651f215c3bb8f0068df7bbf8e03e27fe
VectorGits <davidoluremi0@gmail.com>
2022-07-04 14:24:46 -0700

    Task 03

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * Main - Prints alphabets in lowercase, then in uppercase ;)
+ *
+ * Return: Always 0 (+ve)
+ */
+int main(void)
+{
+	char leet;
+
+	for (leet = 'a'; leet <= 'a'; leet++)
+		putchar(leet);
+	for (leet = 'A'; leet <= 'Z'; leet++)
+		putchar(leet);
+
+	putchar('\n');
+
+	return (0);
+}
