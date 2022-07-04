1652c6e7fdc6be8a03cda6269f4eb40201119445
VectorGits <davidoluremi0@gmail.com>
2022-07-04 14:03:56 -0700

    Task 02

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Entry - point
+ * main - Entry - Prints the alphabets in lowercase.
  *
  * Return: Always 0 (+ve)
  */
@@ -14,7 +14,8 @@ int main(void)
 
 	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
-		putchar('\n');
+
+	putchar('\n');
 
 	return (0);
 }
