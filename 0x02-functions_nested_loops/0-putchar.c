c88c14469616d435b2429a11c92b3a6ebc7a5518
VectorGits <davidoluremi0@gmail.com>
2022-07-05 05:38:49 -0700

    Task 00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,20 @@
  * main - Check description
  * Description: It prints _putchar, followed by a new line.
  *
- * Return: 0.
+ * Return: Always 0.
  */
 
 int main(void)
 {
-	char word[8] = "_putchar";
-	int i;
-
-	for (i = 0; i < 9; i++)
-		_putchar(word[i]);
-	-putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
