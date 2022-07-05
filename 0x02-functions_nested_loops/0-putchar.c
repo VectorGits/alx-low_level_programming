1584f560d0e078bdd68e939e5b51277fd98c9c2e
VectorGits <davidoluremi0@gmail.com>
2022-07-05 05:01:05 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * This script prints _putchar followed by a new line
+ *
+ * Return: On success - 1.
+ * On error, -1 is returned.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
