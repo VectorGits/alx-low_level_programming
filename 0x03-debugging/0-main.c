e5ba069396e2493c291f61bba35bdcaaa9777c4e
VectorGits <davidoluremi0@gmail.com>
2022-07-06 04:02:46 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if int is +ve or -ve
+ *
+ * Return: Should be 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This directory focuses on debugging - An essential skill every aspiring or expert programmer must have, along with a duck ;)
