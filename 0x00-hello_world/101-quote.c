4274a3537c2af8041869096507677267969c7944
Vector <davidoluremi0@gmail.com>
2023-06-16 18:33:11 -0700

    Modified task 8's file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 1 (+ve)
+ * Return: Always 1 (Error)
  */
+
 int main(void)
 {
-	write(STDOUT_FILENO, "and that peice of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
