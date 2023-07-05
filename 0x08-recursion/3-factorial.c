44ed09eaf67633025184c3396fea79d0fe4b909f
Vector <davidoluremi0@gmail.com>
2023-07-05 16:29:09 -0700

    update task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,13 +4,13 @@
  * @n: Random Integer
  *
  * Return: Factorial of n
+ * -1 if n < 0
  */
 int factorial(int n)
 {
-	if (n == 0)
-	{
-		return 1;
-	}
-
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (1);
 	return n * factorial(n - 1);
 }
