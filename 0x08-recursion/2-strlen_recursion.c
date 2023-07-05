cdf22ff429bbb2399c9b5873cbafb440d9ec6bab
Vector <davidoluremi0@gmail.com>
2023-07-05 15:34:40 -0700

    Changed file 1 name and fixed bugs in file 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
  * _strlen_recursion - Returns the lenght of a string
- * @s: pointer variable
- * @i: Random variable
+ * @s: pointer to string.
  *
- * Return lenght of string
+ * Return: lenght of string.
  */
-
 int _strlen_recursion(char *s)
 {
 	int i = 0;
+
 	if (*s != '\0')
 	{
 		i = _strlen_recursion(s + 1);
