54afb3d41eb9a20356dfc1a423f9a3d3c4f814db
Vector <davidoluremi0@gmail.com>
2023-07-07 13:08:16 -0700

    Fixed bugs

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -11,12 +11,13 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
-	int i = n
-		for (; r < i r++)
+	int i = n;
+
+		for (; r < i; r++)
 		{
 			dest[r] = src[r];
 			n--;
 		}
-		return*(dest);
+		return (dest);
 }
 
