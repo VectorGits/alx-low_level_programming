25d6c12b58036b77d716c827640ff7f97723ced9
Vector <davidoluremi0@gmail.com>
2023-07-09 00:55:26 -0700

    Fixed betty bugs

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,14 @@
 
 /**
  * main - function that prints the number of args passed into it
+ * @argc: argument counter
+ * @argv: argument vector
  *
- *
+ * Return: return 0 (success)
  */
 int main(int argc, char *argv[])
 {
-	print("%s\n", argc[]);
+	print("%d\n", argc - 1);
 	(void)argc;
 	return (0);
 }
