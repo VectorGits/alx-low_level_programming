79c873762fdb88d4a2e7a95c262ff7d38e18a4b6
Vector <davidoluremi0@gmail.com>
2023-07-09 00:46:01 -0700

    Added files for Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the number of args passed into it
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	print("%s\n", argc[]);
+	(void)argc;
+	return (0);
+}
