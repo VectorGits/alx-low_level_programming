9f323a0bceb9a01dfb73997413cc1116ac77d3b0
Vector <davidoluremi0@gmail.com>
2023-07-11 11:02:18 -0700

    Added file for Task 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space
+ * in memory which contains a copy of the string passed.
+ *
+ * @str: pointer to string being duplicated
+ *
+ * Return: NULL if string == NULL.
+ * pointer to duplicated string on success
+ * NULL if memory is insufficient.
+ */
+
+char *_strdup(char *str)
+{
+	char *nstr;
+	unsigned int len, i;
+
+	/* check for NULLness of string */
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	nstr = malloc(sizeof(char) * (len + 1));
+
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,5 +2,6 @@
 #define main
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif
