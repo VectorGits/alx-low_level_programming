b3e18eaf2a65c8cace819e06bd6496633b7b3ff3
Vector <davidoluremi0@gmail.com>
2023-07-11 12:24:28 -0700

    Added files for task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include "stdlib.h"
+#include "stdio.h"
+/**
+ * alloc_grid - Returns a pointer to a 2D array of ints.
+ * @width: Width of matrix
+ * @height: height of matrix
+ *
+ * Return: return a pointer of pointer or null
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, a, b;
+	int **p;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		p = (int **) malloc(height * sizeof(int *));
+		/* We gotta make a malloc per pointer */
+		if (!p)
+		{
+			free(p);
+			return (NULL);
+		}
+		for (i = 0; i < height; i++)
+		{
+			p[i] = (int *) malloc(width * sizeof(int));
+			if (!p[i])
+			{
+				for (j = 0; j <= i; j++)
+					free(p[j]);
+				free(p);
+				return (NULL);
+			}
+		}
+		for (a = 0; a < height; a++)
+		{
+			for (b = 0; b < width; b++)
+			{
+				p[a][b] = 0;
+			}
+		}
+		return (p);
+	}
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 0-create_array - Creates an array of chars, and initializes it with a specific char.
 1-strdup - Returns a pointer to a newly allocated space in memory which contains a copy of the string passed.
 2-str_concat - Concatenates two strings.
+3-alloc_grid - Create a 2D matrix using malloc.
