816ee76d3db703977cb9f357e77baa8b4a15e4cc
Vector <davidoluremi0@gmail.com>
2023-07-20 23:56:02 -0700

    Updated code

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - A function that prints a name
+ * print_name - Prints a name
  *
  * @name: The input string
  * @f: The input function to print the name
+ *
+ * Return: void.
  */
 
 void print_name(char *name, void (*f)(char *))
