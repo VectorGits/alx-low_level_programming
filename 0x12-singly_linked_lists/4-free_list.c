9738cd6bedc64a917aa81d16a13d09c0deea80b7
VectorGits <davidoluremi0@gmail.com>
2023-07-30 15:27:19 -0700

    Initial addition of task 4 file

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: Head node.
+ * Return: Nuffin :)
+ */
+
+void free_list(list_t *head);
+{
+	list_t *cur = head;
+
+	while (cur != NULL)
+	{
+		head = head->next;
+		free(cur->str);
+		free(cur);
+	}
+}
