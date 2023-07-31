cc9e3ba9c9d57e796d7bf8ba2321bbe5035c0b2a
VectorGits <davidoluremi0@gmail.com>
2023-07-30 18:22:48 -0700

    Test

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,9 @@
  *
  * Return: Nuffin :^)
  */
-void first() __attribute__((constructor));
+void first(void) __attribute__((constructor));
 
-void first()
+void first(void)
 {
 	printf("You're beat! and yet, you must allow");
 	printf(", \nI bore my house upon my back!\n");
