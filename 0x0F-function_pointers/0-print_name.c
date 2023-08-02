b70f27403f6eaf7c6df0a99fbb6cf15a2269d33f
VectorGits <davidoluremi0@gmail.com>
2023-08-02 02:13:39 -0700

    Added include header

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+
 /**
   *print_name - prints name.
   *@name: pointer to name.
@@ -7,6 +9,6 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name || f)
 		f(name);
 }
