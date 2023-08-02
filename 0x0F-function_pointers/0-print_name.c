84b43df52a53616b97e2046988c21fd7b8d41ebd
VectorGits <davidoluremi0@gmail.com>
2023-08-02 02:24:43 -0700

    modded file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+
 /**
   *print_name - prints name.
   *@name: pointer to name.
@@ -7,6 +9,9 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL && f == NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+
 }
