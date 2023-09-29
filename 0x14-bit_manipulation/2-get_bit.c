e4ae5b9f837e565531f3642dacff92e297b67af7
VectorGits <davidoluremi0@gmail.com>
2023-09-29 13:58:55 -0700

    Debugged file

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,21 +2,21 @@
 /**
  * get_bit - This func gives the value of bit at given index.
  * @n: No to be printed in binary.
+ * @index: index of bit value.
  *
  * Return: Value of error or (-1) if error occurs.
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int n;
-	unsigned int index;
+	unsigned long int mask;
 
 	if (index >= sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
 
-	unsigned long int mask = 1 << index;
+	mask = 1UL << index;
 
 	if ((n & mask) != 0)
 	{
