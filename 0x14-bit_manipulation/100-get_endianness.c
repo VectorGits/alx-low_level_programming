6112b28d7113dc4ffabaee8453181d67f48615e1
VectorGits <davidoluremi0@gmail.com>
2023-09-30 14:19:37 +0100

    fixed func name

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * get_endianess - This func sets the value of bit at given index to 1.
+ * get_endianness - This func sets the value of bit at given index to 1.
  *
  * Return: 0 if big endian else 1.
  */
-int get_endianess(void)
+int get_endianness(void)
 {
 	unsigned int numx = 1;
 	char *numx_ptr = (char *)&numx;
