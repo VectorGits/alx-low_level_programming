7258311de0f5cfb257fc9f7ab42994a907e2bf67
VectorGits <davidoluremi0@gmail.com>
2023-09-30 12:53:40 +0100

    Added file func for task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * flip_bits - This func returns the number of flips to change numbers.
+ * @n: No to be printed in binary.
+ * @index: index of bit value.
+ *
+ * Return: Value of index or (-1) if error occurs.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int mask;
+	int count = 0;
+
+	mask = n ^ m;
+
+	while (mask > 0)
+	{
+		if (mask & 1)
+		{
+			count++;
+		}
+		mask >>= 1;
+		
+	}
+	return (count);
+	
+}
