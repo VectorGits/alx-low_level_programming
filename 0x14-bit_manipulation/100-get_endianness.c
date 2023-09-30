a4f473f6010b1dfe47f7b15617daa1ee5eb98eb3
VectorGits <davidoluremi0@gmail.com>
2023-09-30 14:12:55 +0100

    swatted bugs and fixed requirements

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,19 +4,12 @@
  * @n: No to be printed in binary.
  * @index: index of bit value.
  *
- * Return: Value of index or (-1) if error occurs.
+ * Return: 0 if big endian else 1.
  */
 int get_endianess(void)
 {
 	unsigned int numx = 1;
 	char *numx_ptr = (char *)&numx;
 
-	if (*numx_ptr == 1)
-	{
-		return (1); /*Lil endian*/
-	}
-	else
-	{
-		return (0); /*Big endian*/
-	}
+	return ((int)*numx_ptr);
 }
