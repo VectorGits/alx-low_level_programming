2c2b1d8ac57af67625b1313ed0d66303a7a140c6
VectorGits <davidoluremi0@gmail.com>
2023-09-30 06:17:24 -0700

    Swatted betty bugs

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
  * get_endianess - This func sets the value of bit at given index to 1.
- * @n: No to be printed in binary.
- * @index: index of bit value.
  *
  * Return: 0 if big endian else 1.
  */
