78371ac59f8f6851dcde17386ad274ce90bf4055
VectorGits <davidoluremi0@gmail.com>
2023-12-30 19:30:04 +0100

    Add _strspn function to calculate the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: String to search
+ * @accept: Bytes to accept
+ * Description: Calculates the length (in bytes) of the initial segment of `s`
+ * which consists entirely of bytes in `accept`.
+ * Return: Number of bytes in the initial segment of `s` which consist only of
+ * bytes from `accept`.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	char *a;
+
+	while (*s)
+	{
+		for (a = accept; *a; a++)
+		{
+			if (*s == *a)
+			{
+				count++;
+				break;
+			}
+			else if (*(a + 1) == '\0')
+			{
+				return (count);
+			}
+		}
+	}
+}
