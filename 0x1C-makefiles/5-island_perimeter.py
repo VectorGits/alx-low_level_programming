fb399e3d44bcb6da06f5c1eb7a7d7ffbbddfa4f8
VectorGits <davidoluremi0@gmail.com>
2024-03-26 17:31:58 +0100

    Refactor island_perimeter function to improve readability and maintainability

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,24 +3,25 @@
 Technical interview type challenge
 """
 
+
 def island_perimeter(grid):
-	"""
-	Calculate the perimeter of an island in a grid.
+    """
+    Calculate the perimeter of an island in a grid.
 
-	Args:
-		grid (list): A 2D grid representing the island.
+    Args:
+        grid (list): A 2D grid representing the island.
 
-	Returns:
-		int: The perimeter of the island.
-	"""
-	perimeter = 0
-	for x in range(len(grid)):
-		for y in range(len(grid[x])):
-			if grid[x][y] == 1:
-				perimeter += 4
-				if x > 0 and grid[x - 1][y] == 1:
-					perimeter -= 2
-				if y > 0 and grid[x][y - 1] == 1:
-					perimeter -= 2
+    Returns:
+        int: The perimeter of the island.
+    """
+    perimeter = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                perimeter += 4
+                if x > 0 and grid[x - 1][y] == 1:
+                    perimeter -= 2
+                if y > 0 and grid[x][y - 1] == 1:
+                    perimeter -= 2
 
-	return perimeter
+    return perimeter
