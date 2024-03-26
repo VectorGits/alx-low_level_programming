b4cc988b62a75713be9b718c51d550ab488b552d
VectorGits <davidoluremi0@gmail.com>
2024-03-26 17:20:26 +0100

    Add island_perimeter function to calculate the perimeter of an island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+def island_perimeter(grid):
+	perimeter = 0
+	for x in range(len(grid)):
+		for y in range(len(grid[x])):
+			if grid[x][y] == 1:
+				perimeter += 4
+				if x > 0 and grid[x - 1][y] == 1:
+					perimeter -= 2
+				if y > 0 and grid[x][y - 1] == 1:
+					perimeter -= 2
+
+	return perimeter
